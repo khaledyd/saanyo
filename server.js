cc08ab290339649ef56a4d78d2c7e8afa58787bd
= <shawaarma0200@gmail.com>
2022-12-19 13:17:09 +0300

    dirname

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import  path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 dotenv.config();
